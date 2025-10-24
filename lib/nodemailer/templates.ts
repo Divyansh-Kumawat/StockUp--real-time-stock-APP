bf0b5245648bce448ba01a1518bf44e69c8c303e
Divyansh Kumawat <153420810+Divyansh-Kumawat@users.noreply.github.com>
2025-10-24 14:51:59 +0530

    Update link from Signalist to StockUp in templates

--- lib/nodemailer/templates.ts
+++ lib/nodemailer/templates.ts
@@ -288,7 +288,7 @@ export const NEWS_SUMMARY_EMAIL_TEMPLATE = `<!DOCTYPE html>
                                 </p>
                                 <p style="margin: 0 0 10px 0; font-size: 14px; line-height: 1.5; color: #CCDADC !important;">
                                     <a href="#" style="color: #CCDADC !important; text-decoration: underline;">Unsubscribe</a> | 
-                                    <a href="https://signalist.app" style="color: #CCDADC !important; text-decoration: underline;">Visit Signalist</a>
+                                    <a href="https://stockup.app" style="color: #CCDADC !important; text-decoration: underline;">Visit StockUp</a>
                                 </p>
                                 <p style="margin: 0; font-size: 14px; line-height: 1.5; color: #CCDADC !important;">
                                     © 2025 StockUp
@@ -497,7 +497,7 @@ export const STOCK_ALERT_UPPER_EMAIL_TEMPLATE = `<!DOCTYPE html>
                                 </p>
                                 <p style="margin: 0 0 10px 0; font-size: 14px; line-height: 1.5; color: #CCDADC !important;">
                                     <a href="#" style="color: #CCDADC !important; text-decoration: underline;">Unsubscribe</a> | 
-                                    <a href="https://signalist.app" style="color: #CCDADC !important; text-decoration: underline;">Visit StockUp</a>
+                                    <a href="https://stockup.app" style="color: #CCDADC !important; text-decoration: underline;">Visit StockUp</a>
                                 </p>
                                 <p style="margin: 0; font-size: 14px; line-height: 1.5; color: #CCDADC !important;">
                                     © 2025 StockUp
